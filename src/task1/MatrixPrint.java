703a8e02ea49da34c793ff33fa8df36a744857b8
Mykola-b12 <bolbam.c.h@gmail.com>
2017-05-18 21:29:31 +0300

    array

--- src/task1/MatrixPrint.java
+++ src/task1/MatrixPrint.java
@@ -5,10 +5,10 @@ package task1;
  */
 public class MatrixPrint {
     public static void main(String[] args) {
-        int[] matrix = new int[10];
+        String matrix[] = {"*", "2", "3", "4", "*", "6", "*", "8", "*", "10", "11", "12", "*", "14", "15", "16", "*", "18", "*", "20", "*", "22", "23", "24", "*"};
+
         for (int i = 0; i < matrix.length; i++) {
-            matrix[i] = i;
+            System.out.println(matrix[i]);
         }
-        System.out.println(matrix);
     }
 }
