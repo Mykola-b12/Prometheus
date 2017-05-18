f78b87e712508c95c6c09ac1614c6cd848697c44
Mykola-b12 <bolbam.c.h@gmail.com>
2017-05-18 21:58:58 +0300

    array, need format

--- src/task1/MatrixPrint.java
+++ src/task1/MatrixPrint.java
@@ -8,7 +8,12 @@ public class MatrixPrint {
         String matrix[] = {"*", "2", "3", "4", "*", "6", "*", "8", "*", "10", "11", "12", "*", "14", "15", "16", "*", "18", "*", "20", "*", "22", "23", "24", "*"};
 
         for (int i = 0; i < matrix.length; i++) {
-            System.out.println(matrix[i]);
+            if (i != 0 && i % 5 == 0) {
+                System.out.println();
+            }
+            System.out.print(matrix[i]);
+
+
         }
     }
 }
